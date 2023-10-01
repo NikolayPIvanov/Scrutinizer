bfa454886c5a58682e85407f7823d021bb9edeb6
Nikolay Ivanov <nick.ivanov98@gmail.com>
2023-10-01 10:09:08 +0300

    Adding latest block number in ksqldb

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,16 +1,13 @@
 import { emitter } from './handlers/blockNumber/BlockNumberEmitter';
 import { bootstrap as bootstrapBlockNumberEventHandler } from './handlers/blockNumber/BlockNumberEventHandler';
-import { bootstrap as bootstrapKafka, bootstrapAdmin } from './messaging/Kafka';
+import { bootstrap as bootstrapKafka } from './messaging/Kafka';
 import { bootstrap as bootstrapTransactionConsumer } from "./messaging/TransactionConsumer";
-import { bootstrap as bootstrapBlockConsumer } from "./messaging/BlockConsumer";
 import { providers } from './providers';
 
 
 (async () => {
     await bootstrapKafka();
-    await bootstrapAdmin();
     await bootstrapTransactionConsumer();
-    await bootstrapBlockConsumer();
 
     bootstrapBlockNumberEventHandler();
 
--- packages/migrator/01_mined_transactions_stream.sql
+++ packages/migrator/01_mined_transactions_stream.sql
@@ -16,24 +16,6 @@ CREATE STREAM mined_transactions (
     value_format='json',
     partitions=3);
 
-CREATE STREAM mined_transactions (
-  blockNumber int,
-  chainId int,
-  blockHash varchar,
-  data varchar,
-  hash varchar,
-  value varchar,
-  `from` varchar,
-  `to` varchar,
-  type int,
-  nonce int,
-  gasLimit int
-  )
-  WITH (
-    kafka_topic='transactions',
-    value_format='json',
-    partitions=3);
-
 
 CREATE TABLE transaction_latest AS
   SELECT hash,
@@ -52,3 +34,59 @@ CREATE TABLE transaction_latest AS
   EMIT CHANGES;
 
 SELECT * FROM transaction_latest;
+
+/*
+{
+	"baseFeePerGas": "6990681193",
+	"difficulty": "0",
+	"extraData": "0x546974616e2028746974616e6275696c6465722e78797a29",
+	"gasLimit": "30000000",
+	"gasUsed": "10006210",
+	"hash": "0x520566e4c4560b8da97de0015e934987cb261fbbff4369b76e5e385c30b056e4",
+	"miner": "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97",
+	"nonce": "0x0000000000000000",
+	"number": 18253970,
+	"parentHash": "0xb03114af51cb73f5d2ff6a254ba7eb4417a18ab0d709b0723590404234c7c637",
+	"timestamp": 1696142555
+}
+*/
+CREATE STREAM blocks (
+  baseFeePerGas varchar,
+  difficulty varchar,
+  extraData varchar,
+  gasLimit varchar,
+  gasUsed varchar,
+  hash varchar,
+  miner varchar,
+  nonce varchar,
+  `number` int,
+  parentHash varchar,
+  `timestamp` int
+  )
+  WITH (
+    kafka_topic='blocks',
+    value_format='json',
+    partitions=3);
+
+CREATE STREAM derived AS
+  SELECT `number`, 'latest' AS la FROM blocks EMIT CHANGES;
+
+CREATE TABLE block_number_latest AS
+  SELECT la,
+         LATEST_BY_OFFSET(`number`) AS `number`
+  FROM derived
+  GROUP BY la
+  EMIT CHANGES;
+
+SELECT * FROM block_number_latest
+
+CREATE TABLE blocks_main AS
+  SELECT `number`,
+         LATEST_BY_OFFSET(parentHash) AS parentHash,
+         LATEST_BY_OFFSET(hash) AS hash,
+         LATEST_BY_OFFSET(`timestamp`) AS `timestamp`
+  FROM blocks
+  GROUP BY `number`
+  EMIT CHANGES;
+
+SELECT *, 'status' as sta FROM blocks_main;
